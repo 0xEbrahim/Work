a886aca685dd5775bb1c0d952c2f175facca78d1
0xEbrahim <elsayedebrahim603@gmail.com>
2024-03-04 17:15:37 +0200

    commit

--- File.py
+++ File.py
@@ -3,7 +3,7 @@ from random import randint
 
 for i in range(1, 365):
     
-    for j in range(0,randint(1,10)):
+    for j in range(0,randint(1,2)):
         d = str(i) + ' days ago'
         with open('file.txt', 'a') as file:
             file.write(d)
